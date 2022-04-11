eb6f1aeffad2a6ff1db4e455acedc54e33b895d0
Aayush <aayushpagare21@gmail.com>
2022-04-11 19:19:37 +0530

    Intial Commit

---
+++ partials/dbconnect.php
@@ -0,0 +1,8 @@
+<?php 
+    $username = "root"; 
+    $password = ""; 
+    $database = "dbuser"; 
+    $server = "localhost"; 
+
+    $con = mysqli_connect($server, $username, $password, $database); 
+?>
---
+++ partials/nav.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        .nav { 
+            font-family: 'Baloo Bhai 2', cursive;
+            font-family: 'Baloo Bhaina 2', cursive;
+        }
+    </style>
+</head>
+
+<body>
+    <nav class="nav navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">E-Secure</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/php/login_system/welcome.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/php/login_system/login.php">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/php/login_system/signup.php">Sign-Up</a>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+</body>
+
+</html>
